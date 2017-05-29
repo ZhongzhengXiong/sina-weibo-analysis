c4c87742ed9d0920d2cb3cd94b343ae849ec3a41
ZhongzhengXiong <tomcatxiong2@gmail.com>
2017-05-29 15:12:15 +0800

    beta1.0

--- Weibo_time_analysis.m
+++ Weibo_time_analysis.m
@@ -4,7 +4,7 @@ month = M(:, 4).';
 day = M(:, 5).';
 hour = M(:, 6).';
 
-% tweet monthly
+% 1号-31号每天微博数量
 dayIndex = 1:31;
 n = hist(day, dayIndex);
 figure();
@@ -12,8 +12,7 @@ plot(dayIndex, n,  '-b.');
 xlabel('日', 'FontSize', 15);
 ylabel('微博数量', 'FontSize', 15);
 
-
-% tweet weekly
+% 周一--周日每天微博数量
 len = size(day, 2);
 week = weekday(datenum(year, month, day) - 1);
 weekIndex = 1:7;
@@ -27,8 +26,7 @@ ax = gca;
 set(ax, 'XTick', weekIndex, 'XTickLabel', weekString, 'FontSize', 10);
 ax.XTickLabelRotation = 45;
 
-
-% tweet daily
+% 0时-23时没小时微博数量
 hourIndex = 0:23;
 n = hist(hour, hourIndex);
 figure();
